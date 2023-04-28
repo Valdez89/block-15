5acac82e99f4247be462d26b564c3299f5b69fcd
Stewart Valdez <valdezstewart@gmail.com>
2023-04-27 17:01:33 -0700

    Created customer JS Object.

---
+++ Froyo_HWK/index.js
@@ -0,0 +1,11 @@
+const customer = {
+    firstName:"Jake",
+    lastName: "Smith",
+    email: "jaeksmih!aol.com",
+    phone: undefined,
+    zipCode: "631",
+    favoriteFlavors: 32,
+    cupSize: "medium",
+    favoriteStore: "Target",
+    firstVisit: false,
+}
