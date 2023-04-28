440863f038f75f7f5dd15d49cacc3e76ba5f8e1d
Stewart Valdez <valdezstewart@gmail.com>
2023-04-27 17:29:00 -0700

    Completed HWK

--- Froyo_HWK/index.js
+++ Froyo_HWK/index.js
@@ -1,3 +1,5 @@
+// Below is an object created for the survey labeled as customer.
+
 const customer = {
     firstName:"Jake",
     lastName: "Smith",
@@ -9,3 +11,31 @@ const customer = {
     favoriteStore: "Target",
     firstVisit: false,
 }
+
+// Using bracket notation to update errors corrected by the customer.
+
+    customer['email'] = "Jak3Smith1992@email.com";
+    customer['phone'] = 3195551234;
+    customer['zipCode'] = "63132";
+    customer['favoriteFlavors'] = ['coffe', 'strawberry','matcha'];
+
+
+// Deleting 2 properties requested from management.
+
+delete customer ['zipCode'];
+delete customer ['favoriteFlavors'];
+
+// Management wants topping, future flavors, and today's purchase cost added.
+// Jake the customer provides the information being entered below for new properties.
+
+const customer['toppings'] = ['chocolate sprinkles', 'wafer straws', and 'gummy bears'];
+const customer['favoriteFlavors'] = ['mango'];
+const customer['todaysPurchaseCost'] = 5.32;
+
+// The following new results will be added to the final survey.
+// The output will be printed through an array.
+
+const keys = Object.keys(customer);
+console.log(keys);
+
+
